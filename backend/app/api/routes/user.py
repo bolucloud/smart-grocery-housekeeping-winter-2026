261c5de028e597ac30ff18cbcfd339cdcc35f22c
lena-A-Al <lena.al.developer@gmail.com>
2026-01-25 11:52:51 -0500

    add get user route

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
 
 from app.db.deps import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserOut
 
 router = APIRouter()
+
+
+@router.get("/user/{user_id}", response_model=UserOut)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
 
 @router.post("/user", response_model=UserOut)
 def create_user(payload: UserCreate, db: Session = Depends(get_db)):
