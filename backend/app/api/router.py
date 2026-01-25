f4c875075db68b1e64ea846ae48998bdf4fe4f0c
lena-A-Al <lena.al.developer@gmail.com>
2026-01-24 23:46:14 -0500

    testing more routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from app.api.routes import health, user
+from app.api.routes import health, user, hello
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, tags=["health"])
 api_router.include_router(user.router, tags=["user"])
+api_router.include_router(hello.router, tags=["hello"])
+
