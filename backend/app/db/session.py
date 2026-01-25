f585512546ebd137f97fac6a2b750ff7fc765ab5
lena-A-Al <lena.al.developer@gmail.com>
2026-01-24 23:06:04 -0500

    adding some comments

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True) #Manages DB connections & pooling
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #Manages transactions & ORM state per request
