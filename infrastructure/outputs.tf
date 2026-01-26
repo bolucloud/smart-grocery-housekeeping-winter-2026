b6ae7f9bc9ec8493a1f9552a37353b81be1ae300
bolucloud <bolu@bolu.cloud>
2026-01-25 20:49:06 -0600

    add container infrastructure

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,4 @@
+output "ecr_repo_url" {
+  value = aws_ecr_repository.smart_grocery_housekeeping_repository.repository_url
+}
+
---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "app_name" {
+    default = "smart-grocery-housekeeping"
+}
+
+variable "ecs_container_port" {
+    default = 80
+}
+
+variable "cpu" {
+    default = "256"
+}
+
+variable "memory" {
+    default = "512"
+}
