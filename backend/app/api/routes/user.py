710db1796b4a21b09cfefe4e671bc054965af66f
John Weathers <weathers.johnb@gmail.com>
2026-01-28 18:24:38 -0500

    Add basic delete, add check to create

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 
@@ -8,7 +9,8 @@ from app.schemas.user import UserCreate, UserOut
 
 router = APIRouter()
 
-
+# TODO: consider updating route pattern after introducing auth to a self-fetch pattern e.g., GET /user
+# instead of GET /user/{user_id} so authentication doesn't depend on fragile additional checks outside of middleware
 @router.get("/{user_id}", response_model=UserOut)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.user_id == user_id).first()
@@ -17,11 +19,34 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     return user
 
 
-
-@router.post("/", response_model=UserOut)
+@router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
 def create_user(payload: UserCreate, db: Session = Depends(get_db)):
     user = User(email=payload.email, display_name=payload.display_name)
     db.add(user)
-    db.commit()
+    # just assuming a 409 right now for simplicity
+    # need to make more robust, potentially check pg constraints
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="A user with that email already exists"
+        )
     db.refresh(user)
     return user
+
+
+# TODO: consider updating route pattern after introducing auth (same logic as above)
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+    # 200 pattern where we return object for now to make for a better demo
+    return {
+        "deleted": user_id
+    }
