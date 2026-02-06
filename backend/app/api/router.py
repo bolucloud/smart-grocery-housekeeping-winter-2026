70726fb6ec45c89ff3236cb4a209b3177b5eaad9
lena-A-Al <lena.al.developer@gmail.com>
2026-02-05 22:28:51 -0500

    working on image recognition using openai

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import health, user
+from app.api.routes import health, user, recognizeItem
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, prefix="/health", tags=["health"])
 api_router.include_router(user.router, prefix="/user",  tags=["user"])
-
-
+api_router.include_router(recognizeItem.router, prefix="/recognizeItem", tags=["recognizeItem"])
