5641e75fca5e1f5d3bd666a315258be9eecc24cd
lena-A-Al <lena.al.developer@gmail.com>
2026-02-14 18:23:59 -0500

    add recognize item to proteected routes

--- backend/app/api/protected_router.py
+++ backend/app/api/protected_router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
 from app.api.deps import get_firebase_claims
 from app.api.routes import user
+from app.api.routes import recognizeItem
 
 private_api_router = APIRouter(
     dependencies=[Depends(get_firebase_claims)]
 )
 
 private_api_router.include_router(user.router, prefix="/user",  tags=["user"])
+private_api_router.include_router(recognizeItem.router, prefix="/recognize-item", tags=["recognize-item"])
