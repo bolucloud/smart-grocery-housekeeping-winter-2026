b4f9b1b4a338208df5cd443430215542032fb448
lena-A-Al <lena.al.developer@gmail.com>
2026-02-14 14:09:01 -0500

    updated

--- backend/app/api/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes import health, user, recognizeItem
-
-api_router = APIRouter()
-
-api_router.include_router(health.router, prefix="/health", tags=["health"])
-api_router.include_router(user.router, prefix="/user",  tags=["user"])
-api_router.include_router(recognizeItem.router, prefix="/recognize-item", tags=["recognize-item"])
