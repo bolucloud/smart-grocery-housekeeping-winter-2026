3d010115c6f917b4aa4a8e773acd5040b24e1820
John Weathers <weathers.johnb@gmail.com>
2026-02-26 10:16:08 -0500

    updates to Firebase init function - use public getter instead of private field - call google.auth.default() to force exception when missing creds

--- backend/app/auth/firebase.py
+++ backend/app/auth/firebase.py
@@ -1,3 +1,5 @@
+import google.auth
+
 import firebase_admin
 from firebase_admin import auth, credentials
 from app.schemas.firebase import FirebaseClaims
@@ -8,9 +10,15 @@ def init_firebase() -> None:
     Initialize Firebase SDK using default credentials.
     For local development define env var GOOGLE_APPLICATION_CREDENTIALS.
     """
-    # unfortunately firebase_admin does not expose a "public" field here
-    # but this check is needed to make the initialization idempotent
-    if not firebase_admin._apps:
+    # get_app() raising a ValueError means the instance does not exist
+    try:
+        firebase_admin.get_app()
+        return
+    except ValueError:
+        # this is here so that an exception will be raised if credentials are missing
+        # this exception is caught in startup, logs some useful info, then raises a RuntimeError
+        google.auth.default()
+        # ApplicationDefault() is still needed to configure the app easily
         firebase_admin.initialize_app(credentials.ApplicationDefault())
 
 
